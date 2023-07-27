4b02a8a78ed6fda7e94b3dbb8a8ef3c15cb3f0af
KSJ-0 <kim.mail1124@gmail.com>
2023-07-27 12:02:06 +0900

    first commit

---
+++ src/main/java/com/example/CITest/CITest.java
@@ -0,0 +1,17 @@
+package com.example.CITest;
+
+import java.util.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class CITest {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/example/CITest/CiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CITest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
